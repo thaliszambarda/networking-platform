6408f59ce21dddb2e681e007c2832904d2f6d01d
thaliszambarda <thaliszambarda.dev@gmail.com>
2025-11-09 16:54:40 -0300

    chore(db): add new fields to members table

---
+++ apps/server/prisma/migrations/20251109171546_update_members_table/migration.sql
@@ -0,0 +1,6 @@
+-- AlterTable
+ALTER TABLE "Member" ADD COLUMN     "address" TEXT,
+ADD COLUMN     "city" TEXT,
+ADD COLUMN     "country" TEXT,
+ADD COLUMN     "phone" TEXT,
+ADD COLUMN     "role" TEXT;
